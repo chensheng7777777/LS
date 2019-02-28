1ee9285893f95a5df433119481a44ea68544fd9d
陈胜 <chenshenghbu@163.com>
2019-02-28 14:47:54 +0800

    123

--- LS.CMS.Common/LSKeys.cs
+++ LS.CMS.Common/LSKeys.cs
@@ -8,6 +8,34 @@ namespace LS.CMS.Common
 {
     public class LSKeys
     {
+        #region SESSION KEYS
 
+        /// <summary>
+        /// 用户SESSION
+        /// </summary>
+        public const string SESSION_USER_INFO = "ls_session_user_info";
+
+        /// <summary>
+        /// 用户登录次数SESSION
+        /// </summary>
+        public const string SESSION_LOGIN_SUM = "ls_session_login_sum";
+
+
+        #endregion
+
+
+
+        #region COOKIE KEYS 
+
+        /// <summary>
+        /// 用户名COOKIE
+        /// </summary>
+        public const string COOKIE_USER_NAME = "ls_cookie_user_name";
+        /// <summary>
+        /// 密码COOKIE
+        /// </summary>
+        public const string COOKIE_PASSWORD = "ls_cookie_password";
+
+        #endregion
     }
 }
--- LS.CMS.Web/admin/login.aspx.cs
+++ LS.CMS.Web/admin/login.aspx.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using LS.CMS.BLL;
+using LS.CMS.Common;
+using LS.CMS.Model;
+
 namespace LS.CMS.Web.admin
 {
     public partial class login : System.Web.UI.Page
@@ -18,6 +22,32 @@ namespace LS.CMS.Web.admin
         {
             string userName = txtUserName.Text;
             string userPwd = txtPassword.Text;
+            if (Session[LSKeys.SESSION_LOGIN_SUM] == null)
+            {
+                Session[LSKeys.SESSION_LOGIN_SUM] = 1;
+            }
+            else
+            {
+                Session[LSKeys.SESSION_LOGIN_SUM] = Convert.ToInt32(Session[LSKeys.SESSION_LOGIN_SUM])+1;
+            }
+            if (Convert.ToInt32(Session[LSKeys.SESSION_LOGIN_SUM])>5)
+            {
+                msgtip.InnerText = "您登录失败已经超过五次,请关闭浏览器重试";
+                return;
+            }
+            ls_user_bll userBLL = new ls_user_bll();
+            ls_user user=userBLL.Login(userName, userPwd);
+            if (user == null)
+            {
+                msgtip.InnerText = "用户名密码错误";
+            }
+            else
+            {
+                Session[LSKeys.SESSION_USER_INFO] = user;
+                Response.Cookies[LSKeys.COOKIE_USER_NAME].Value = userName;
+                Response.Cookies[LSKeys.COOKIE_PASSWORD].Value = user.user_password;
+                Response.Redirect("index.aspx");
+            }
         }
     }
 }
