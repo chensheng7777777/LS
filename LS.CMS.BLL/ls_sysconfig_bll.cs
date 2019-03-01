797081bb544d263db7986e127ff0c9f9ededbe29
陈胜 <chenshenghbu@163.com>
2019-03-01 13:03:04 +0800

    系统配置信息

---
+++ LS.CMS.BLL/ls_sysconfig_bll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LS.CMS.BLL
+{
+    /// <summary>
+    /// 系统配置业务逻辑类
+    /// </summary>
+    public class ls_sysconfig_bll
+    {
+
+    }
+}
---
+++ LS.CMS.DAL/ls_sysconfig_dal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LS.CMS.Model;
+using LS.CMS.Common;
+
+
+namespace LS.CMS.DAL
+{
+    public class ls_sysconfig_dal
+    {
+        private static object lockHelper = new object();
+
+        /// <summary>
+        /// 读取站点配置文件
+        /// </summary>
+        /// <param name="configPath"></param>
+        /// <returns></returns>
+        public ls_sysconfig LoadConfig(string configPath)
+        {
+            return (ls_sysconfig)SerializationHelper.Load(typeof(ls_sysconfig),configPath);
+        }
+        /// <summary>
+        /// 写入站点配置文件
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="configFilePath"></param>
+        public void SaveConfig(ls_sysconfig config,string configFilePath)
+        {
+            lock (lockHelper)
+            {
+                SerializationHelper.Save(config, configFilePath);
+            }
+        }
+        
+
+    }
+}
